0910c7e2e95fc76a70db466bd05a304f6d1fdaf3
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-04-29 11:49:35 +0200

    modif images

--- Tableau_de_bord_rza_app.py
+++ Tableau_de_bord_rza_app.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.write("Bienvenue sur le Tableau de Bord Science Ouverte du RZA")
 
-st.image(r"data\images\nuage-de-mots.png")
+#st.image(r"data\images\nuage-de-mots.png")
+st.image("data\images\logo_CNRS.png")
+st.image("data\images\logo_RZA.png")
