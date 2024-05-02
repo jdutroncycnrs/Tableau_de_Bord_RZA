b7d30b3ea050241fa805557faaadc03015ad0506
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-05-02 15:30:48 +0200

    prepa dates GN indores

--- pages/2_GN.py
+++ pages/2_GN.py
@@ -22,12 +22,44 @@ liste_ZAs= ['ZA1','ZA2','ZA3']
 Selection_ZA= st.sidebar.multiselect(label="Zones Ateliers", options=liste_ZAs)
 
 ##################################### LECTURE DATA ###########################################
-data = pd.read_csv("pages/data/Enregistrements_190424.csv")
+data = pd.read_csv("pages/data/Enregistrements_RZA_020524.csv")
 data.rename(columns={"createDate":"Date"}, inplace=True)
 
 ##################################### TRAITEMENT PREALABLE DATES###################################
-#data.dropna(subset="Date" ,inplace=True)
-data['Date'] = pd.to_datetime(data['Date'], format='mixed', utc=True)
+dico = {'Jan':'01',
+        'Feb':'02',
+        'Mar':'03',
+        'Apr':'04',
+        'May':'05',
+        'Jun':'06',
+        'Jul':'07',
+        'Aug':'08',
+        'Sep':'09',
+        'Oct':'10',
+        'Nov':'11',
+        'Dec':'12'}
+dico2 = {' 1,':' 01',
+         ' 2,':' 02',
+         ' 3,':' 03',
+         ' 4,':' 04',
+         ' 5,':' 05',
+         ' 6,':' 06',
+         ' 7,':' 07',
+         ' 8,':' 08',
+         ' 4,':' 09'}
+for i in range(len(data)):
+    for term, num in dico.items():
+        data.loc[i,'Date'] = data.loc[i,'Date'].replace(term,num).replace('@','/')
+    for term, num in dico2.items():
+        data.loc[i,'Date'] = data.loc[i,'Date'].replace(term,num).replace(',','')
+    data.loc[i,'Date'] = re.split('/',data.loc[i,'Date'])[0][0:-1].replace(' ','-')+re.split('/',data.loc[i,'Date'])[1]
+
+data['Date'] = pd.to_datetime(data['Date'], format='%m-%d-%Y %H:%M:%S.%f', utc=True)
+
+######################## AVEC MON GN LOCAL #############################
+#data['Date'] = pd.to_datetime(data['Date'], format='mixed', utc=True) 
+########################################################################
+
 data.sort_values(by="Date", inplace=True)
 data.loc[:,'Compte_cumulé']=np.arange(len(data))+1
 data['Year']=0
@@ -36,7 +68,7 @@ for i in range(len(data)):
 
 data_bis =data.copy()
 data_bis.set_index('Date', inplace=True)
-data_resampled =data_bis.resample(rule="ME").size()
+data_resampled =data_bis.resample(rule="3D").size()
 liste_dates = data_resampled.index.values
 liste_comptes = data_resampled.values
 df = pd.DataFrame([liste_dates,liste_comptes], index=['Date','Compte_mensuel']).T
