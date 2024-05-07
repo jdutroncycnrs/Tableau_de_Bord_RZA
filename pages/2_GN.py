e83605cf3232310b57d76905bf01d054afd25a4d
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-05-07 12:05:44 +0200

    test bokeh

--- pages/2_GN.py
+++ pages/2_GN.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import re
-import plotly.graph_objects as go
-import plotly.express as px
+from bokeh.plotting import figure, show
 pd.options.mode.chained_assignment = None
 
 ########### TITRE DE L'ONGLET ######################################
@@ -240,5 +239,16 @@ cnt_orga = df_orga.value_counts()[0:10]
 df_orga = pd.DataFrame(cnt_orga)
 #st.bar_chart(df_orga)
 
-fig = px.bar(df_orga)
-st.plotly_chart(fig)
+fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+counts = [5, 3, 4, 2, 4, 6]
+
+p = figure(x_range=fruits, height=350, title="Fruit Counts",
+           toolbar_location=None, tools="")
+
+p.vbar(x=fruits, top=counts, width=0.9)
+
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+
+#show(p)
+st.bokeh_chart(p)
