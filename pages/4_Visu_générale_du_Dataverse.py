a44925f74a670969b1a8c476602d06f7d968b89d
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-07-22 14:23:43 +0200

    idem

--- pages/4_Visu_générale_du_Dataverse.py
+++ pages/4_Visu_générale_du_Dataverse.py
@@ -111,19 +111,16 @@ if len(Selection_ZA)!=0:
         my_bar = st.progress(0, text=progress_text)
         for i in range(len(Selection_ZA)):
             time.sleep(0.1)
+            s = int(data['ids_niv2'][data['niv2']==Selection_ZA[i]].iloc[0])
+            cpt = 0
             try:
-                s = int(data['ids_niv2'][data['niv2']==Selection_ZA[i]].values)
-                cpt = 0
-                try:
-                    datav_contenu = Recup_contenu_dataverse(api,s)
-                    for j in range(len(datav_contenu['data'])):
-                        try:
-                            identifieur = datav_contenu["data"][j]['identifier']
-                            cpt +=1
-                        except:
-                            pass
-                except:
-                    pass
+                datav_contenu = Recup_contenu_dataverse(api,s)
+                for j in range(len(datav_contenu['data'])):
+                    try:
+                        identifieur = datav_contenu["data"][j]['identifier']
+                        cpt +=1
+                    except:
+                        pass
             except:
                 pass
             Nombre_depots.append(cpt)
