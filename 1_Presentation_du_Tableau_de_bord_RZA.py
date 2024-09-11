9a9fee2937bc72565e19852d99078085bf467832
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-09-11 14:56:39 +0200

    maj

--- 1_Presentation_du_Tableau_de_bord_RZA.py
+++ 1_Presentation_du_Tableau_de_bord_RZA.py
@@ -72,3 +72,45 @@ Nuage_mots = Image.open("nuage-de-mots.png")
 left_co,center_co,last_co = st.columns(3)
 with center_co:
     st.sidebar.image(Nuage_mots, width=300)
+
+
+tit = 'Documentations'
+s_tit= f"<p style='font-size:50px;color:rgb(140,140,140)'>{tit}</p>"
+st.markdown(s_tit,unsafe_allow_html=True)
+
+FAIR_principes = Image.open("Principes_FAIR.png")
+st.image(FAIR_principes, width=1000)
+
+tit = 'Référents "Données" du RZA'
+s_tit= f"<p style='font-size:50px;color:rgb(140,140,140)'>{tit}</p>"
+st.markdown(s_tit,unsafe_allow_html=True)
+
+st.markdown('Jérôme Dutroncy, LTSER-FR Réseau des Zones Ateliers jerome.dutroncy@univ-smb.fr')
+
+st.markdown('Cécile Pignol , Zone Atelier Alpes - cecile.pignol@univ-smb.fr')
+
+st.markdown('A venir, Zone Atelier Arc Jurassien - ')
+
+st.markdown('Françoise Le Moal ,  Zone Atelier Armorique - francoise.le-moal@univ-rennes1.fr')
+
+st.markdown('Anne Clemens , Zone Atelier Bassin du Rhône - anne.clemens@graie.org')
+
+st.markdown('A venir,  Zone Atelier Brest Iroise - ')
+
+st.markdown('Isabelle Charpentier , Zone Atelier Environnementale Urbaine - icharpentier@unistra.fr')
+
+st.markdown('Simon Chamaillé , Zone Atelier Hwange - simon.chamaille@cefe.cnrs.fr')
+
+st.markdown('Annaelle Simonneau ,  Zone Atelier Loire - anaelle.simonneau@univ-orleans.fr')
+
+st.markdown('Emmanuelle Montarges  , Zone Atelier Bassin de la Moselle - emmanuelle.montarges@univ-lorraine.fr')
+
+st.markdown('Wilfired Heintz , Zone Atelier Pyrénées Garonne - wilfried.heintz@inrae.fr')
+
+st.markdown('Marie Silvestre , Zone Atelier Seine - marie.silvestre@sorbonne-universite.fr')
+
+st.markdown('David Sarramia , Zone Atelier Territoires Uranifères - david.sarramia@clermont.in2p3.fr')
+
+st.markdown('Iris Barjhoux , Zone Atelier Environnementale Rurale - iris.barjhoux@univ-reims.fr')
+
+st.markdown('Jean-Claude Raynal  , Zone Atelier Santé Environnement Camargue - jean-claude.raynal@cnrs.fr')
