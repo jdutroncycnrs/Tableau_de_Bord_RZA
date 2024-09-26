63ef5b1b17732403666c2273d5ad53905353b387
jdutroncycnrs <jerome.dutroncy@univ-savoie.fr>
2024-09-26 21:42:10 +0200

    correction groupes

--- tempo.py
+++ tempo.py
@@ -1,27 +1,13 @@
-#authority = "10.48579"
-#st.write(liste_identifiers_dataset)
-        
-#for identifier in liste_identifiers_dataset:
-#    dataset = api.get_dataset(identifier=f"doi:{authority}/{identifier}")
-#    dataset_ = dataset.json()
-#    st.write(dataset_["data"]["latestVersion"]["metadataBlocks"]["citation"]["fields"][0]["value"])     
+"""import pandas as pd
 
-import pandas as pd
+liste_zam = ["info:doi:10.24396%2FORDAR-6","info:doi:10.24396%2FORDAR-5","info:doi:10.24396%2FORDAR-8","info:doi:10.24396%2FORDAR-7","info:doi:10.24396%2FORDAR-19","info:doi:10.24396%2FORDAR-112","info:doi:10.24396%2FORDAR-110","info:doi:10.24396%2FORDAR-116","info:doi:10.24396%2FORDAR-115","info:doi:10.24396%2FORDAR-114","info:doi:10.24396%2FORDAR-113","info:doi:10.24396%2FORDAR-119","info:doi:10.24396%2FORDAR-67","info:doi:10.24396%2FORDAR-128","info:doi:10.24396%2FORDAR-72","info:doi:10.24396%2FORDAR-73","info:doi:10.24396%2FORDAR-71","info:doi:10.24396%2FORDAR-76","info:doi:10.24396%2FORDAR-77","info:doi:10.24396%2FORDAR-74","info:doi:10.24396%2FORDAR-75","info:doi:10.24396%2FORDAR-78","info:doi:10.24396%2FORDAR-79","info:doi:10.24396%2FORDAR-139","info:doi:10.24396%2FORDAR-143","info:doi:10.24396%2FORDAR-148","info:doi:10.24396%2FORDAR-58","info:doi:10.24396%2FORDAR-59","info:doi:10.24396%2FORDAR-56","info:doi:10.24396%2FORDAR-62"]
 
-data = {
-    'GEMET - INSPIRE themes': 444,
-    'theme.EnvironnementFR.rdf,GEMET - INSPIRE themes,theme.thesaurus_costel.rdf': 81,
-    'GEMET - INSPIRE themes,Vocabulaire MétaZABR': 48,
-    'theme.EnvironnementFR.rdf,GEMET - INSPIRE themes': 35,
-    'Régions administratives de France': 26,
-    'theme.EnvironnementFR.rdf,theme.thesaurus_costel.rdf': 23,
-    'GEMET - INSPIRE themes,GEMET - Concepts,sea regions of the world.,Régions administratives de France,countries,Continents': 23,
-    'GEMET - INSPIRE themes,Continents,sea regions of the world.,countries,Régions administratives de France': 16,
-    'GEMET - Concepts,GEMET - INSPIRE themes,Régions de France,Region': 15,
-    'GEMET - Concepts,GEMET - INSPIRE themes': 14
-}
+df_groups = pd.read_csv("pages/data/infos_MD2/infos_groupes.csv",index_col=[0])
 
-# Convert it into a DataFrame
-df = pd.DataFrame(list(data.items()), columns=['Themes', 'Counts'])
+for i in range(len(df_groups)):
+    if df_groups.loc[i,"Identifiant"] in liste_zam:
+        df_groups.loc[i,"Groupe"]="zam"
 
-print(df)
+df_groups.to_csv("pages/data/infos_MD2/infos_groupes.csv")
+
+print(len(df_groups[df_groups['Groupe']=="zam"]))"""
